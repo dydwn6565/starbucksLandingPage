6ff9a7c0e65f016ce0e1c725325da64850cf24ae
YongjuLee <ivan.yongju.lee@gmail.com>
2023-08-23 21:49:33 -0700

    updated image animation (based on the scrolled position)

--- js/index.js
+++ js/index.js
@@ -61,7 +61,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  console.log(slideIndex)
+  console.log(slideIndex);
   if (slides.length - slideIndex === 1) {
     slides[slideIndex - 1].style.order = 1;
     slides[slideIndex].style.order = 2;
@@ -133,7 +133,7 @@ const observer = new IntersectionObserver(
           `${entry.target.className.split(" ")[0]}-show`
         );
 
-        console.log(entry);
+        // console.log(entry);
       } else {
         entry.target.classList.remove(
           `${entry.target.className.split(" ")[0]}-show`
@@ -149,3 +149,217 @@ const observer = new IntersectionObserver(
 images.forEach((image) => {
   observer.observe(image);
 });
+
+let animationTriggered = false;
+let animationTriggeredSeasonalCoffee = false;
+let animationTriggeredReserveCoffee = false;
+let animationTriggeredFavorite = false;
+let animationTriggeredFind = false;
+function animateBox() {
+  if (!animationTriggered) {
+    document
+      .getElementById("visual-spoon-image")
+      .animate([{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }], {
+        duration: 6000,
+      });
+    document
+      .getElementById("carabel-crumble-image")
+      .animate([{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }], {
+        duration: 5000,
+      });
+    document
+      .getElementById("oatmeal-latte-image")
+      .animate([{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }], {
+        duration: 4000,
+      });
+
+    animationTriggered = true;
+  }
+}
+
+function animateBoxiceCoffee() {
+  // console.log(document.documentElement.scrollTop);
+  if (
+    1400 <= document.documentElement.scrollTop &&
+    !animationTriggeredSeasonalCoffee
+  ) {
+    document
+      .getElementById("season-product-img")
+      .animate(
+        [
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2500,
+        }
+      );
+    document
+      .getElementById("season-product-img-top")
+      .animate(
+        [
+          { opacity: 0 },
+          { opacity: 0, marginRight: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2500,
+        }
+      );
+    document
+      .getElementById("season-product-img-bottom")
+      .animate(
+        [
+          { opacity: 0 },
+          { opacity: 0, marginRight: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 3000,
+        }
+      );
+    animationTriggeredSeasonalCoffee = true;
+  }
+  if (
+    1700 <= document.documentElement.scrollTop &&
+    !animationTriggeredReserveCoffee
+  ) {
+    document
+      .getElementById("reserve-coffee-logo")
+      .animate(
+        [
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2500,
+        }
+      );
+    document
+      .getElementById("reserve-coffee-reserve-coffee")
+      .animate(
+        [
+          { opacity: 0 },
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2500,
+        }
+      );
+    document
+      .getElementById("reserve-cofee-description")
+      .animate(
+        [
+          { opacity: 0 },
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 3000,
+        }
+      );
+    animationTriggeredReserveCoffee = true;
+  }
+  if (
+    1950 <= document.documentElement.scrollTop &&
+    !animationTriggeredFavorite
+  ) {
+    document
+      .getElementById("favorite-title")
+      .animate([{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }], {
+        duration: 2000,
+      });
+    document
+      .getElementById("favorite-second")
+      .animate([{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }], {
+        duration: 2000,
+      });
+    document
+      .getElementById("favorite-third")
+      .animate([{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }], {
+        duration: 3000,
+      });
+    document
+      .getElementById("favorite-forth")
+      .animate([{ opacity: 0 }, { opacity: 0 }, { opacity: 1 }], {
+        duration: 3100,
+      });
+    animationTriggeredFavorite = true;
+  }
+
+  if (3400 <= document.documentElement.scrollTop && !animationTriggeredFind) {
+    document
+      .getElementById("find-store-first-image")
+      .animate(
+        [
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2000,
+        }
+      );
+    document
+      .getElementById("find-store-second-image")
+      .animate(
+        [
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginRight: "0px" },
+        ],
+        {
+          duration: 2000,
+        }
+      );
+    document
+      .getElementById("find-store-description-first")
+      .animate(
+        [
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2000,
+        }
+      );
+    document
+      .getElementById("find-store-description-second")
+      .animate(
+        [
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2000,
+        }
+      );
+    document
+      .getElementById("find-store-description-second-container")
+      .animate(
+        [
+          { opacity: 0, marginLeft: "-200px" },
+          { opacity: 1 },
+          { marginLeft: "0px" },
+        ],
+        {
+          duration: 2000,
+        }
+      );
+
+
+    animationTriggeredFind = true;
+  }
+}
+animateBox();
+
+document.addEventListener("scroll", animateBoxiceCoffee);
